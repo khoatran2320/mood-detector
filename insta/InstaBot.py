386c5882715632af47eb50e5c67f23dbf00192f3
Khoa Tran <kttran2320@gmail.com>
2020-01-24 12:30:25 -0500

    restructured code

--- insta/InstaBot.py
+++ insta/InstaBot.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 from time import sleep
+import os
+
 
 class InstaBot:
-    def __init__(self,  username, password):
+    def __init__(self):
         self.driver = webdriver.Chrome()
-        self.username = username
-        self.password = password
+        self.username = os.environ.get('INSTA_USER')
+        self.password = os.environ.get('INSTA_PASS')
         self.driver.get("https://instagram.com")
         sleep(2)
         self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]").click()
         sleep(2)
-        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(self.username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(self.password)
         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         sleep(4)
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
         sleep(2)
+
+InstaBot()
